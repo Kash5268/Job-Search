3bcd872ec521f6f729289c7a081ac18b2adcbb5e
Kash5268 <kalmahanbetalihan@gmail.com>
2023-06-26 20:11:31 +0600

    commit

---
+++ utils/index.js
@@ -0,0 +1,10 @@
+export const checkImageURL = (url) => {
+  if (!url) return false;
+  else {
+    const pattern = new RegExp(
+      "https:\\/\\/.+\\.(png|jgp|jpeg|bmp|gif|webp)$",
+      "i"
+    );
+    return pattern.test(url);
+  }
+};
